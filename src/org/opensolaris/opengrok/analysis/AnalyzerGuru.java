88242e9a956535534b0b4947713ff6c4f7c45761
Chris Fraire <cfraire@me.com>
2018-01-28 09:58:47 -0600

    ... I S S U E # 6 0 7 : Add classes to support Forth analysis

--- src/org/opensolaris/opengrok/analysis/AnalyzerGuru.java
+++ src/org/opensolaris/opengrok/analysis/AnalyzerGuru.java
@@ -78,6 +78,7 @@ import org.opensolaris.opengrok.analysis.erlang.ErlangAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.executables.ELFAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.executables.JarAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.executables.JavaClassAnalyzerFactory;
+import org.opensolaris.opengrok.analysis.forth.ForthAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.fortran.FortranAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.golang.GolangAnalyzerFactory;
 import org.opensolaris.opengrok.analysis.haskell.HaskellAnalyzerFactory;
@@ -309,7 +310,8 @@ public class AnalyzerGuru {
                 new PascalAnalyzerFactory(env),
                 new AdaAnalyzerFactory(env),
                 new RubyAnalyzerFactory(env),
-                new EiffelAnalyzerFactory(env)
+                new EiffelAnalyzerFactory(env),
+                new ForthAnalyzerFactory(env)
             };
 
             for (FileAnalyzerFactory analyzer : analyzers) {
@@ -342,7 +344,7 @@ public class AnalyzerGuru {
      * {@link FileAnalyzerFactory} subclasses are revised to target more or
      * different files.
      * @return a value whose lower 32-bits are a static value
-     * 20171230_00
+     * 20180209_00
      * for the current implementation and whose higher-32 bits are non-zero if
      * {@link #addExtension(java.lang.String, org.opensolaris.opengrok.analysis.FileAnalyzerFactory)}
      * or
@@ -350,7 +352,7 @@ public class AnalyzerGuru {
      * has been called.
      */
     public long getVersionNo() {
-        final int ver32 = 20171230_00; // Edit comment above too!
+        final int ver32 = 20180209_00; // Edit comment above too!
         long ver = ver32;
         if (customizationHashCode != 0) {
             ver |= (long)customizationHashCode << 32;
