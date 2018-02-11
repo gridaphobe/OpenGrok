fd4980012b1cb317e9968d63da8bb144a7d5aaeb
Chris Fraire <cfraire@me.com>
2018-02-10 19:09:56 -0600

    Support ctags for historical revisions
    - Revise BoundedBlockingObjectPool as LIFO-to-FIFO
      so the webapp does not start extraneous
      instances; Indexer switches to FIFO performance
      when the queue pool is emptied.
    - Add Repository.getHistoryGet() override to
      allow sub-classes to override to avoid a full
      in-memory version; and override for Git and
      Mercurial.

--- src/org/opensolaris/opengrok/history/HistoryGuru.java
+++ src/org/opensolaris/opengrok/history/HistoryGuru.java
@@ -260,6 +260,26 @@ public final class HistoryGuru {
     }
 
     /**
+     * Gets a named revision of the specified file into the specified target.
+     *
+     * @param target a require target file
+     * @param parent The directory containing the file
+     * @param basename The name of the file
+     * @param rev The revision to get
+     * @return {@code true} if content was found
+     * @throws java.io.IOException if an I/O error occurs
+     */
+    public boolean getRevision(File target, String parent, String basename,
+        String rev) throws IOException {
+
+        Repository rep = getRepository(new File(parent));
+        if (rep != null) {
+            return rep.getHistoryGet(target, parent, basename, rev);
+        }
+        return false;
+    }
+
+    /**
      * Get a named revision of the specified file.
      *
      * @param parent The directory containing the file
